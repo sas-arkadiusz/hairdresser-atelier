8952f0c63d37ef1646bb64cd866e085fbb798412
Arkadiusz Sas <kontakt.sas.arkadiusz@gmail.com>
2020-02-06 00:31:22 +0100

    Initial commit

---
+++ hairdresser-app/src/main/java/com/endev/hairdresserapp/HairdresserAppApplication.java
@@ -0,0 +1,14 @@
+package com.endev.hairdresserapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairdresserAppApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello!");
+		SpringApplication.run(HairdresserAppApplication.class, args);
+	}
+
+}
---
+++ hairdresser-app/src/test/java/com/endev/hairdresserapp/HairdresserAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endev.hairdresserapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HairdresserAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
