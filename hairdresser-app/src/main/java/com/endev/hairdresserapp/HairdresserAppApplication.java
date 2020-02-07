ef0cd49e4594c00c8c0b2e9267a62417cc4768c9
Arkadiusz Sas <kontakt.sas.arkadiusz@gmail.com>
2020-02-07 01:03:30 +0100

    Created docker-compose and API dockerfile to containerize the application

--- hairdresser-app/src/main/java/com/endev/hairdresserapp/HairdresserAppApplication.java
+++ hairdresser-app/src/main/java/com/endev/hairdresserapp/HairdresserAppApplication.java
@@ -2,12 +2,19 @@ package com.endev.hairdresserapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HairdresserAppApplication {
 
+	@RequestMapping("/")
+	public String home() {
+		return "Hello, Docker!";
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Hello!");
 		SpringApplication.run(HairdresserAppApplication.class, args);
 	}
 
